3d411f10c49023badb06cc9a13df40895e505bc5
leovieir <86242282+leovieir@users.noreply.github.com>
2021-07-25 23:27:09 -0300

    Add files via upload

---
+++ Exercicio6/Par.java
@@ -0,0 +1,20 @@
+public class Par<F, S>{
+  private F first;
+  private S second;
+
+  public F getFirst(){
+    return this.first;
+  }
+
+  public void setFirst(F first){
+    this.first = first;
+  }
+
+  public S getSecond(){
+    return this.second;
+  }
+
+  public void setSecond(S second){
+    this.second = second;
+  }
+}
